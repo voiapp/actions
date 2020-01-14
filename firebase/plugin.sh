c63889d719a048baae36bc3db1db6e90449c37f3
drpytho <filip@voiapp.io>
2020-01-14 15:52:55 +0100

    Allow action to pass --only argument

--- firebase/plugin.sh
+++ firebase/plugin.sh
@@ -7,5 +7,9 @@ export GOOGLE_APPLICATION_CREDENTIALS=/service_account.json
 cd $INPUT_CONTEXT
 
 firebase use $INPUT_PROJECT
-firebase deploy
+if [ -z "$INPUT_ONLY_ARG" ]; then 
+    firebase deploy
+else
+    firebase deploy --only $INPUT_ONLY_ARG
+fi
 
